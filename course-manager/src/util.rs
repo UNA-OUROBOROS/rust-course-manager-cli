acab38e9ca5c6b9e39e0a31349c0c17323e86b32
Jonathan Guzmán <jonguz5@hotmail.com>
2023-01-31 20:06:07 -0600

    remove unused util.rs

--- course-manager/src/util.rs
+++ course-manager/src/util.rs
@@ -8,29 +8,6 @@ fn app_name() -> String {
     "course-manager".to_string()
 }
 
-/// Returns the path of the app user config directory
-/// if it does not exist it will be created
-///
-/// | Error              | Description                           |
-/// | ------------------ | ------------------------------------- |
-/// | UserDirNotFound    | The user directory could not be found |
-/// | CouldNotCreatePath | The app path could not be created     |
-fn get_app_config_dir() -> Result<std::path::PathBuf, error::Error> {
-    let app_dir = dirs::config_dir()
-        .ok_or(error::Error::UserDirNotFound)?
-        .join(app_name());
-    if !app_dir.exists() {
-        let res = std::fs::create_dir(&app_dir);
-        if res.is_err() {
-            return Err(error::Error::CouldNotCreatePath(
-                app_dir,
-                res.err().unwrap(),
-            ));
-        }
-    }
-    Ok(app_dir)
-}
-
 /// Returns the path of the app user data directory
 /// this is where application data is stored such as
 /// the translation files, resource files, etc.
